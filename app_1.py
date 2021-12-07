2937a7fa6d0d4daaaa55a7be49fc035755945ad8
evgeshabalyka <evgeshabalyka66@gmail.com>
2021-12-07 20:42:13 +0200

    Initial commit

--- app_1.py
+++
@@ -1,5 +0,0 @@
-# var_1 = "volume1"
-# var_2 = ["v","o","l","u","m","e","1"]
-# var_3 = ("v","o","l","u","m","e","1", var_2)
-# print("o" in var_3)
-X = i
--- brocode.py
+++ brocode.py
@@ -280,6 +280,20 @@ print(var_3)
 
 -----------------------------------------------------
 
+date: int = 1278
+
+text = "Hello new year"
+
+text_1 = "Hello " + str(date) + " year"
+
+text_2 = "Hello {y} year {x}".format(x=date, y="any")
+
+text_3 = f"Hello {date=}  \\new year"
+
+print("Hello " , date_5 + , year")
+
+-----------------------------------------------------
+
 from car import Car
 
 car_1 = Car("Chevy","Corvette","2021","blue")
